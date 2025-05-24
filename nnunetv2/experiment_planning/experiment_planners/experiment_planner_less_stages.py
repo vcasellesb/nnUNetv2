8b34554dc291e938779efa6bc80d493490794929
Vicent Caselles <vcasellesb@uoc.edu>
2025-05-24 11:31:57 +0200

    experimentplanner less stages

--- nnunetv2/experiment_planning/experiment_planners/experiment_planner_less_stages.py
+++ nnunetv2/experiment_planning/experiment_planners/experiment_planner_less_stages.py
@@ -11,6 +11,25 @@ from nnunetv2.experiment_planning.experiment_planners.residual_unets.residual_en
 #   * La Rosa uses 20ish
 #   
 
+class ExperimentPlannerLessStages(ExperimentPlanner):
+    def __init__(self, dataset_name_or_id: Union[str, int],
+                 gpu_memory_target_in_gb: float = 8,
+                 preprocessor_name: str = 'DefaultPreprocessor', plans_name: str = 'nnUNetLessStagesPlans',
+                 overwrite_target_spacing: Union[List[float], Tuple[float, ...]] = None,
+                 suppress_transpose: bool = False, 
+                 UNet_featuremap_min_edge_length: int = 8):
+                
+        super().__init__(dataset_name_or_id, gpu_memory_target_in_gb, 
+                         preprocessor_name, plans_name, overwrite_target_spacing, 
+                         suppress_transpose)
+        
+        print(f'Using a custom experiment planner, with name "{self.__class__.__name__}", which has a '
+                  f'custom minimum number of feature map edge length (at the lowest resolution). '
+                  f'It has been set to: {UNet_featuremap_min_edge_length}.')
+        
+        self.UNet_featuremap_min_edge_length = UNet_featuremap_min_edge_length
+
+
 class nnUNetPlannerResEncMLessStages(nnUNetPlannerResEncM):
     def __init__(self, dataset_name_or_id: Union[str, int],
                  gpu_memory_target_in_gb: float = 8,
@@ -26,4 +45,5 @@ class nnUNetPlannerResEncMLessStages(nnUNetPlannerResEncM):
         print(f'Using a custom experiment planner, with name "{nnUNetPlannerResEncMLessStages.__name__}", which has a '
               f'custom minimum number of feature map edge length (at the lowest resolution). '
               f'It has been set to: {UNet_featuremap_min_edge_length}.')
+        
         self.UNet_featuremap_min_edge_length = UNet_featuremap_min_edge_length
