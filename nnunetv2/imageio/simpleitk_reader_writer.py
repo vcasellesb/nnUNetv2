2e77ecabc3d96f9140435eae1f54062018f8f3c6
Vicent Caselles <vcasellesb@uoc.edu>
2025-07-14 14:37:59 +0200

    eliminate unused import

--- nnunetv2/imageio/simpleitk_reader_writer.py
+++ nnunetv2/imageio/simpleitk_reader_writer.py
@@ -13,7 +13,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from typing import Tuple, Union, List, Iterable
+from typing import Tuple, Union, List
 from functools import partial
 import numpy as np
 from nnunetv2.imageio.base_reader_writer import BaseReaderWriter
